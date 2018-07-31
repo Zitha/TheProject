8612a2a08afffbefa6a0b0cc8bed53818d34fba4
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2018-08-01 00:20:38 +0200

    download files

---
+++ TheProject.ReportGenerator/GenerateReport.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheProject.Data;
+using TheProject.Model;
+using System.IO;
+using System.Net.Http;
+using System.Data.Entity;
+
+namespace TheProject.ReportGenerator
+{
+    public class GenerateReport
+    {
+
+        public Dictionary<string, string> GenerateAllReport()
+        {
+            //List<string> list = new List<string>();
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            FacilityReport facilityReport = new FacilityReport();
+            ApplicationUnit unit = new ApplicationUnit();
+
+            List<Facility> facilities = unit.Facilities.GetAll()
+                                        .Include(b => b.Buildings)
+                                        .Include(d => d.DeedsInfo)
+                                        .Include(c => c.Portfolio)
+                                        .Include(p => p.ResposiblePerson)
+                                        .Include(l => l.Location)
+                                        .ToList();
+
+            List<Facility> facilitiffes = facilities.Where(f => f.Status == "Submitted").ToList();
+
+            int i = 0;
+            foreach (var facility in facilitiffes)
+            {
+                if (i < 30)
+                {
+                    string facilityLocation = facilityReport.GenerateInvoice(facility);
+                    dictionary.Add(facilityLocation, facility.ClientCode);
+                    i++;
+                }
+            }
+
+            return dictionary;
+        }
+
+        public string GenerateOneReport(string clientCode)
+        {
+            FacilityReport facilityReport = new FacilityReport();
+            ApplicationUnit unit = new ApplicationUnit();
+
+            List<Facility> facilities = unit.Facilities.GetAll()
+                                        .Include(b => b.Buildings)
+                                        .Include(d => d.DeedsInfo)
+                                        .Include(c => c.Portfolio)
+                                        .Include(p => p.ResposiblePerson)
+                                        .Include(l => l.Location)
+                                        .ToList();
+
+            Facility facility = facilities.Where(f => f.ClientCode.Trim().ToLower() == clientCode.Trim().ToLower()).FirstOrDefault();
+
+            int i = 0;
+            if (facility != null)
+            {
+                string facilityLocation = facilityReport.GenerateInvoice(facility);
+                return facilityLocation;
+            }
+            else {
+                return null;
+            }
+        }
+
+    }   
+}
--- TheProject.ReportWebApplication/Controllers/FacilityController.cs
+++ TheProject.ReportWebApplication/Controllers/FacilityController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TheProject.ReportWebApplication.Models;
 using TheProject.ReportWebApplication.Services;
+using TheProject.ReportGenerator;
+using System.IO.Compression;
 
 namespace TheProject.ReportWebApplication.Controllers
 {
@@ -31,17 +34,56 @@ namespace TheProject.ReportWebApplication.Controllers
         }
 
         // GET: Facility/Edit/5
-        [HttpPost]
         public ActionResult DownloadFacility([Bind(Include = "ClientCode")] Facility facility)
         {
-            return View();
+            if (string.IsNullOrEmpty(facility.ClientCode))
+            {
+                ModelState.AddModelError("", "Please Enter Client Code.");
+                return RedirectToAction("Index", "Facility");
+            }
+            else {
+                GenerateReport generateReport = new GenerateReport();
+                string fullPath = generateReport.GenerateOneReport(facility.ClientCode);
+                if (string.IsNullOrEmpty(fullPath))
+                {
+                    ModelState.AddModelError("", "Please Enter Client Code.");
+                    return RedirectToAction("Index", "Facility");
+                }
+                else {
+                    byte[] fileBytes = GetFile(fullPath);
+                    return File(fileBytes, "application/pdf", facility.ClientCode + ".pdf");
+                }               
+            }
+           
+        }
+
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
         }
 
         // GET: Facility/Edit/5
         [HttpPost]
-        public ActionResult DownloadAllFacility()
+        public FileResult DownloadAllFacility()
         {
-            return View();
+            GenerateReport generateReport = new GenerateReport();
+            Dictionary<string, string> fullPathList = generateReport.GenerateAllReport();
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var ziparchive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    foreach (var item in fullPathList)
+                    {
+                        ziparchive.CreateEntryFromFile(item.Key, item.Value + ".pdf");
+                    }
+                }
+                return File(memoryStream.ToArray(), "application/zip", "facilities.zip");
+            }
         }
         #endregion
     }
